cedffb4041fe42965359b6191716514ed5e008a9
Haindrich Zoltán (kirk) <kirk@rxd.hu>
2016-06-27 23:05:28 +0200

    add debug level control

--- serialotad/src/main.cpp
+++ serialotad/src/main.cpp
@@ -12,6 +12,7 @@
 
 #include <boost/program_options.hpp>
 #include <boost/log/trivial.hpp>
+#include <boost/log/core.hpp>
 
 //#include <boost/iostreams/device/file_descriptor.hpp>
 //#include <boost/iostreams/stream.hpp>
@@ -29,6 +30,8 @@ const int BUF_SIZ =1024;
 #include "stk500.h"
 #include "Stk500Emulator.h"
 
+#include <boost/log/expressions.hpp>
+
 int main(int ac, char**av) {
 
 	// Declare the supported options.
@@ -36,6 +39,9 @@ int main(int ac, char**av) {
 	desc.add_options()("help", "produce help message");
 	desc.add_options()("compression", po::value<int>(),
 			"set compression level");
+	int debugLevel;
+	desc.add_options()("debug,d",po::value<int>(&debugLevel)->default_value(0),"debug level");
+
 
 	po::variables_map vm;
 	po::store(po::parse_command_line(ac, av, desc), vm);
@@ -45,6 +51,10 @@ int main(int ac, char**av) {
 		cout << desc << "\n";
 		return 1;
 	}
+
+	boost::log::core::get()->set_filter (
+	    boost::log::trivial::severity >= boost::log::trivial::warning-debugLevel
+	    );
 
 	SerialChannel sp("/dev/arduino_mega_75237333536351E00181");
 	PtyChannel pt1;
