af2ed9fa738f17803b81e2aa8def6a9308fa1875
Haindrich Zoltán (kirk) <kirk@rxd.hu>
2016-07-12 00:06:30 +0200

    add some damped lum read to desk prog; add nonlinearity of strip ... its ok

--- dev/desk/desk.ino
+++ dev/desk/desk.ino
@@ -14,12 +14,15 @@ int ledPin = 9;    // LED connected to digital pin 9
 
 class KitchenSensorDebugService {
 public:
+	double	lightAggr=0.0;
 	void init(){
 	}
 	void ack(){
 		showState(krf.packet.kitchen);
 	}
 	void showState(KRF::Packet_Kitchen&packet){
+        lightAggr=lightAggr*0.8+0.2*packet.state.lum;
+
         Serial.print(F("x T:"));
         Serial.print(packet.state.temp);
         Serial.print(F("  H:"));
@@ -30,6 +33,8 @@ public:
         Serial.print(packet.state.lum);
         Serial.print(F("  S:"));
         Serial.print(packet.state.strip_bright);
+        Serial.print(F("  LA:"));
+        Serial.print(lightAggr);
         Serial.println();
 	}
 
@@ -88,13 +93,13 @@ void loop() {
 	krf.listen(500,[&] {
 		channel_kitchen.service_rx(SERVICE_KITCHEN, kss);
 		channel_leddrv.service_rx(SERVICE_KITCHEN, ldrvs);
-        Serial.print("# rx ");
+//        Serial.print("# rx ");
         Serial.print(krf.packet.hdr.seqId);
         Serial.print(" ");
         Serial.println(krf.packet.hdr.destination);
 	});
 
-	if(freeWheel==0)
+	if(freeWheel%32)
 	channel_leddrv.service_tx(SERVICE_KITCHEN, ldrvs);
 
 	freeWheel++;
@@ -104,8 +109,20 @@ void loop() {
 		case 'v':
 			ldrvs.add(1);
 		break;
+		case 'b':
+			ldrvs.add(8);
+		break;
+		case 'n':
+			ldrvs.add(32);
+		break;
 		case 'c':
 			ldrvs.add(-1);
+		break;
+		case 'x':
+			ldrvs.add(-8);
+		break;
+		case 'z':
+			ldrvs.add(-32);
 		break;
 		default:
 			break;
