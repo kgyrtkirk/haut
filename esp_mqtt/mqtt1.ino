f32a84d368735258a98f8876cadfa1b04a4405a8
Haindrich Zoltán (kirk) <kirk@rxd.hu>
2016-11-03 22:18:23 +0100

    add more things to esp

--- esp_mqtt/mqtt1.ino
+++ esp_mqtt/mqtt1.ino
@@ -25,7 +25,12 @@
 
 #include <ESP8266WiFi.h>
 #include <PubSubClient.h>
+#include <ESP8266WiFiMulti.h>
+#include <ESP8266HTTPClient.h>
+#include <ESP8266httpUpdate.h>
+#include <stdlib.h>
 
+#define FET_PIN	4
 // Update these with values suitable for your network.
 
 const char* ssid = "kroute";
@@ -33,6 +38,7 @@ fix_passwd
 const char* password = "fixed_later";
 const char* mqtt_server = "192.168.128.33";
 
+ESP8266WiFiMulti WiFiMulti;
 WiFiClient espClient;
 PubSubClient client(espClient);
 long lastMsg = 0;
@@ -44,12 +50,14 @@ void setup_wifi() {
   delay(10);
   // We start by connecting to a WiFi network
   Serial.println();
+  Serial.println("Welcome to the disclaimer2! ");
   Serial.print("Connecting to ");
   Serial.println(ssid);
+fix_passwd
+  WiFiMulti.addAP("kroute", "fkfkf");
+//  WiFi.begin(ssid, password);
 
-  WiFi.begin(ssid, password);
-
-  while (WiFi.status() != WL_CONNECTED) {
+  while (WiFiMulti.run() != WL_CONNECTED) {
     delay(500);
     Serial.print(".");
   }
@@ -72,6 +80,33 @@ void callback(char* topic, byte* payload, unsigned int length) {
   Serial.println();
 
   // Switch on the LED if an 1 was received as first character
+  if ((char)payload[0] == 'U') {
+      Serial.println("attempting upgrade");
+      t_httpUpdate_return ret = ESPhttpUpdate.update("http://192.168.128.33:8181/update2.php","wow");
+      //t_httpUpdate_return  ret = ESPhttpUpdate.update("https://server/file.bin");
+
+      switch(ret) {
+          case HTTP_UPDATE_FAILED:
+              Serial.printf("HTTP_UPDATE_FAILD Error (%d): %s\n", ESPhttpUpdate.getLastError(), ESPhttpUpdate.getLastErrorString().c_str());
+              break;
+
+          case HTTP_UPDATE_NO_UPDATES:
+        	  Serial.println("HTTP_UPDATE_NO_UPDATES");
+              break;
+
+          case HTTP_UPDATE_OK:
+        	  Serial.println("HTTP_UPDATE_OK");
+              break;
+      }
+  }
+  if ((char)payload[0] == 'A') {
+	  int val=strtol(((char*)payload)+1,0,10);
+	  analogWrite(FET_PIN,val);
+      char msg[128];
+      sprintf (msg, "analog: %d", val);
+      client.publish("outTopic", msg);
+
+  }
   if ((char)payload[0] == '1') {
     digitalWrite(BUILTIN_LED, LOW);   // Turn the LED on (Note that LOW is the voltage level
     // but actually the LED is on; this is because
@@ -93,7 +128,7 @@ void reconnect() {
     if (client.connect(clientId.c_str())) {
       Serial.println("connected");
       // Once connected, publish an announcement...
-      client.publish("outTopic", "hello world");
+      client.publish("outTopic", "XH3ll0 World");
       // ... and resubscribe
       client.subscribe("inTopic");
     } else {
@@ -106,12 +141,33 @@ void reconnect() {
   }
 }
 
+#include "DHT.h"
+#define DHTPIN 5
+#define DHTTYPE DHT22   // DHT 22  (AM2302), AM2321
+//#define DHTTYPE DHT21   // DHT 21 (AM2301)
+
+// Connect pin 1 (on the left) of the sensor to +5V
+// NOTE: If using a board with 3.3V logic like an Arduino Due connect pin 1
+// to 3.3V instead of 5V!
+// Connect pin 2 of the sensor to whatever your DHTPIN is
+// Connect pin 4 (on the right) of the sensor to GROUND
+// Connect a 10K resistor from pin 2 (data) to pin 1 (power) of the sensor
+
+// Initialize DHT sensor.
+// Note that older versions of this library took an optional third parameter to
+// tweak the timings for faster processors.  This parameter is no longer needed
+// as the current DHT reading algorithm adjusts itself to work on faster procs.
+DHT dht(DHTPIN, DHTTYPE);
+
 void setup() {
+	  pinMode(16, INPUT);     // Initialize the BUILTIN_LED pin as an output
   pinMode(BUILTIN_LED, OUTPUT);     // Initialize the BUILTIN_LED pin as an output
   Serial.begin(115200);
   setup_wifi();
   client.setServer(mqtt_server, 1883);
   client.setCallback(callback);
+  dht.begin();
+  analogWrite(FET_PIN,16);
 }
 
 void loop() {
@@ -125,7 +181,13 @@ void loop() {
   if (now - lastMsg > 2000) {
     lastMsg = now;
     ++value;
-    sprintf (msg, "hello world #%d", value);
+    int hum=(dht.readHumidity()*100);
+    int temp=(dht.readTemperature()*100);
+//    hum=3;
+    if(digitalRead(16)>0)
+    	sprintf (msg, "XHEllo world+#%d %d: %d", value, temp,hum);
+    else
+    	sprintf (msg, "Xhello world-#%d %d: %d", value, temp,hum);
     Serial.print("Publish message: ");
     Serial.println(msg);
     client.publish("outTopic", msg);
