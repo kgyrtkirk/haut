9961cc506aabc80fc4d4749191b0ed4d21e6ca2d
Zoltan Haindrich <kirk@rxd.hu>
2018-11-18 22:48:42 +0100

    use c++ library

--- dev/shutter_ctl/paho.cpp
+++ dev/shutter_ctl/paho.cpp
@@ -12,35 +12,54 @@
 #define QOS         1
 #define TIMEOUT     10000L
 
+#include <iostream>
+#include <cstdlib>
+#include <string>
+#include <map>
+#include <vector>
+#include <cstring>
+#include "mqtt/client.h"
+
 int main(int argc, char* argv[])
 {
-    MQTTClient client;
-    MQTTClient_connectOptions conn_opts = MQTTClient_connectOptions_initializer;
-    MQTTClient_message pubmsg = MQTTClient_message_initializer;
-    MQTTClient_deliveryToken token;
-    int rc;
+//    const std::string TOPIC { "hello" };
+    const std::string PAYLOAD1 { "Hello World!" };
 
-    MQTTClient_create(&client, ADDRESS, CLIENTID,
-        MQTTCLIENT_PERSISTENCE_NONE, NULL);
-    conn_opts.keepAliveInterval = 20;
-    conn_opts.cleansession = 1;
+    const char* PAYLOAD2 = "Hi there!";
 
-    if ((rc = MQTTClient_connect(client, &conn_opts)) != MQTTCLIENT_SUCCESS)
-    {
-        printf("Failed to connect, return code %d\n", rc);
-        exit(-1);
+    // Create a client
+
+    mqtt::client cli(ADDRESS, CLIENTID);
+
+    mqtt::connect_options connOpts;
+    connOpts.set_keep_alive_interval(20);
+    connOpts.set_clean_session(true);
+
+    try {
+        // Connect to the client
+
+        cli.connect(connOpts);
+
+        // Publish using a message pointer.
+
+        auto msg = mqtt::make_message(TOPIC, PAYLOAD1);
+        msg->set_qos(QOS);
+
+        cli.publish(msg);
+
+        // Now try with itemized publish.
+
+        cli.publish(TOPIC, PAYLOAD2, strlen(PAYLOAD2), 0, false);
+
+        // Disconnect
+
+        cli.disconnect();
     }
-    pubmsg.payload = (void*)PAYLOAD;
-    pubmsg.payloadlen = strlen(PAYLOAD);
-    pubmsg.qos = QOS;
-    pubmsg.retained = 0;
-    MQTTClient_publishMessage(client, TOPIC, &pubmsg, &token);
-    printf("Waiting for up to %d seconds for publication of %s\n"
-            "on topic %s for client with ClientID: %s\n",
-            (int)(TIMEOUT/1000), PAYLOAD, TOPIC, CLIENTID);
-    rc = MQTTClient_waitForCompletion(client, token, TIMEOUT);
-    printf("Message with delivery token %d delivered\n", token);
-    MQTTClient_disconnect(client, 10000);
-    MQTTClient_destroy(&client);
-    return rc;
+    catch (const mqtt::exception& exc) {
+        std::cerr << "Error: " << exc.what() << " ["
+            << exc.get_reason_code() << "]" << std::endl;
+        return 1;
+    }
+
+    return 0;
 }
