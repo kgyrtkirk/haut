2df0e97a8b82fd84dd263f28b93d697ed08ad706
Zoltan Haindrich <kirk@rxd.hu>
2019-06-18 01:13:54 +0200

    format; include

--- dev/sonoff_blink/Blink.ino
+++ dev/sonoff_blink/Blink.ino
@@ -1,29 +1,26 @@
 /*
-Blink LED - Sonoff Basic
-*/
+ Blink LED - Sonoff Basic
+ */
 
-
+#include "Arduino.h"
 // Pin config
 int LED_SONOFF = 13;
 
-
-
-
 void setup() {
-  // initialise digital pin LED_SONOFF as an output.
-  pinMode(LED_SONOFF, OUTPUT);
+	// initialise digital pin LED_SONOFF as an output.
+	pinMode(LED_SONOFF, OUTPUT);
 }
 
-void b(int BLINK_DURATION){
-	  digitalWrite(LED_SONOFF, LOW); // LOW will turn on the LED
-	  delay(BLINK_DURATION);
-	  digitalWrite(LED_SONOFF, HIGH); // HIGH will turn off the LED
-	  delay(BLINK_DURATION);
+void b(int BLINK_DURATION) {
+	digitalWrite(LED_SONOFF, LOW); // LOW will turn on the LED
+	delay(BLINK_DURATION);
+	digitalWrite(LED_SONOFF, HIGH); // HIGH will turn off the LED
+	delay(BLINK_DURATION);
 
 }
 
 void loop() {
 	b(100);
 	b(100);
-	  delay(600);
+	delay(600);
 }
