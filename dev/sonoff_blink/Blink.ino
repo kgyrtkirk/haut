c34799d26b8340c1e304371b4f5ae95187edcb10
Zoltan Haindrich <kirk@rxd.hu>
2019-06-18 01:13:26 +0200

    sonoff-blink

---
+++ dev/sonoff_blink/Blink.ino
@@ -0,0 +1,29 @@
+/*
+Blink LED - Sonoff Basic
+*/
+
+
+// Pin config
+int LED_SONOFF = 13;
+
+
+
+
+void setup() {
+  // initialise digital pin LED_SONOFF as an output.
+  pinMode(LED_SONOFF, OUTPUT);
+}
+
+void b(int BLINK_DURATION){
+	  digitalWrite(LED_SONOFF, LOW); // LOW will turn on the LED
+	  delay(BLINK_DURATION);
+	  digitalWrite(LED_SONOFF, HIGH); // HIGH will turn off the LED
+	  delay(BLINK_DURATION);
+
+}
+
+void loop() {
+	b(100);
+	b(100);
+	  delay(600);
+}
