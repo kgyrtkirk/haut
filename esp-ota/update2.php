a3454a4997caf18b12db3bc4ccc3f67fa456170a
Zoltan Haindrich <kirk@rxd.hu>
2019-12-10 16:08:57 +0100

    make it work; test ota

--- esp-ota/update2.php
+++ esp-ota/update2.php
@@ -67,6 +67,7 @@ $db = array (
         "DC:4F:22:37:7B:9F" => "sonoff_basic",      # sonoff.basic #1
         "BC:DD:C2:0E:04:C1" => "sonoff_basic",      # sonoff.basic #2
         "84:0D:8E:A5:7F:AA" => "esp_shutterctl",    # breadboard esp8266 module
+	"2C:F4:32:17:3C:0D" => "esp_gen3p",         # nodemcu (amica)
 );
 
 if(!isset($db[$_SERVER['HTTP_X_ESP8266_STA_MAC']])) {
@@ -78,7 +79,10 @@ exit();
 $fwName = $db[$_SERVER['HTTP_X_ESP8266_STA_MAC']];
 
 //$localBinary = sprintf("%s/%s/Release/%s.bin",$workspace,$fwName,$fwName);
-$localBinary = sprintf("%s/prod/%s/Release/%s.bin",$workspace,$fwName,$fwName);
+//$localBinary = sprintf("%s/prod/%s/Release/%s.bin",$workspace,$fwName,$fwName);
+$localBinary = sprintf("%s/%s/.pio/build/nodemcuv2/firmware.bin",$workspace,$fwName);
+
+
 error_log("serving binary from path:");
 error_log("$localBinary");
 
--- esp_gen3p/src/main.cpp
+++ esp_gen3p/src/main.cpp
@@ -1,38 +1,123 @@
 #include "Arduino.h"
 #include "k-espcore.h"
 #include "DelayControlValue.h"
+
+
+// Pin config
+int LED_SONOFF = 2;
+
+KMqttClient	kmqtt;
+
+#include "DHT.h"
+DHT dht(16, DHT22);
+
+#include "DelayControlValue.h"
 DelayControlValue<uint8_t, 8> lampCtrl;
+#include <Wire.h>
 
-/**
- * Blink
- *
- * Turns on an LED on for one second,
- * then off for one second, repeatedly.
- */
+int lampVal=0;
+void setLamp(int val, bool force);
 
-#ifndef LED_BUILTIN
-#define LED_BUILTIN 13
-#endif
+void 	moveShutter(int idx, byte* payload, unsigned int length) {
+	if(length>=10) {
+		return;
+	}
+	char	tmp[10];
+	strncpy(tmp,(char*)payload,length);
+	tmp[length]=0;
 
-void setup()
-{
-  // initialize LED digital pin as an output.
-  pinMode(LED_BUILTIN, OUTPUT);
+	lampVal=atoi(tmp);
+	setLamp(lampVal, true);
 }
 
-void loop()
-{
-  // turn the LED on (HIGH is the voltage level)
-  digitalWrite(LED_BUILTIN, HIGH);
 
-int tAll=600;
-int tOn=500;
-  // wait for a second
-  delay(tAll-tOn);
+void c0(char* topic, byte* payload, unsigned int length) {
+	moveShutter(0,payload, length);
+}
 
-  // turn the LED off by making the voltage LOW
-  digitalWrite(LED_BUILTIN, LOW);
+void setup() {
+	// initialise digital pin LED_SONOFF as an output.
+	pinMode(LED_SONOFF, OUTPUT);
+	lampCtrl.command(0, 0, 0);
+	Serial.begin(115200);
+	kmqtt.init("dev");
+	kmqtt.subscribe("dev/light",&c0);
 
-   // wait for a second
-  delay(tOn);
+
+	dht.begin();
+	pinMode(A0, INPUT);
+
+	Wire.pins(12, 13);
+	Wire.begin();
+
+}
+
+int last_lamp_val = -1;
+void setLamp(int val, bool force) {
+
+//	if (last_lamp_val == val && !force)
+	//	return;
+	last_lamp_val = val;
+	Wire.beginTransmission(0x33);
+	Wire.write(val);
+	Wire.write(16);
+	Wire.write(16);
+	Wire.endTransmission();
+}
+
+class PrometheusExporterStream {
+
+	void write(const String&name, const String&type,const String&value) {
+
+	}
+public:
+	void writeGauge(const String&name, int value) {
+		write(name, "gauge", String(value));
+	}
+
+};
+
+struct PromValues {
+	int	humidity;
+	int temperature;
+
+	void sendValues(PrometheusExporterStream&pp) {
+		pp.writeGauge("haut_humidity", humidity);
+		pp.writeGauge("haut_temperature", temperature);
+	}
+};
+
+void everySecond(){
+	int hum;
+	int temp;
+	hum = (dht.readHumidity() * 100);
+	temp = (dht.readTemperature() * 100);
+	int lum = analogRead(A0);
+
+	int pir = digitalRead(5);
+	printf("temp@:	%d 	hum:	%d	lum:	%d	pir:	%d\r\n",hum,temp,lum,pir);
+	kmqtt.publishMetric("@/humidity",hum);
+	kmqtt.publishMetric("@/temperature",temp);
+	kmqtt.publishMetric("@/pir",pir);
+	kmqtt.publishMetric("@/lum",lum);
+	kmqtt.publishMetric("@/uptime",millis());
+
+	// to make sure the slave's wdt is happy; retransmit lampVal
+	setLamp(lampVal, false);
+
+
+
+
+}
+
+
+uint64_t	next_t=0;
+void loop() {
+	kmqtt.loop();
+//	kmqtt.blink();
+	uint64_t	now=millis();
+	if(now > next_t) {
+		next_t=millis()+1000;
+		everySecond();
+	}
 }
