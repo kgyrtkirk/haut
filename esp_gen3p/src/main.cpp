0fdf12e564c9303aaf2b09144abdf6b6dcad6f7a
Zoltan Haindrich <kirk@rxd.hu>
2019-12-10 15:27:33 +0100

    configure local libs

--- esp_gen3p/src/main.cpp
+++ esp_gen3p/src/main.cpp
@@ -1,10 +1,13 @@
+#include "Arduino.h"
+#include "DelayControlValue.h"
+DelayControlValue<uint8_t, 8> lampCtrl;
+
 /**
  * Blink
  *
  * Turns on an LED on for one second,
  * then off for one second, repeatedly.
  */
-#include "Arduino.h"
 
 #ifndef LED_BUILTIN
 #define LED_BUILTIN 13
@@ -21,12 +24,14 @@ void loop()
   // turn the LED on (HIGH is the voltage level)
   digitalWrite(LED_BUILTIN, HIGH);
 
+int tAll=600;
+int tOn=500;
   // wait for a second
-  delay(1000);
+  delay(tAll-tOn);
 
   // turn the LED off by making the voltage LOW
   digitalWrite(LED_BUILTIN, LOW);
 
    // wait for a second
-  delay(1000);
+  delay(tOn);
 }
