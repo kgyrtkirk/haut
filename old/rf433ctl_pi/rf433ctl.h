09db3911c9efd58541f6c7d709cb41176437684b
Zoltan Haindrich <kirk@rxd.hu>
2019-12-10 14:49:08 +0100

    add leftout header

---
+++ old/rf433ctl_pi/rf433ctl.h
@@ -0,0 +1,25 @@
+#pragma once
+
+#include <wiringPi.h>
+#include <stdint.h>
+//#include "Arduino.h"
+#include "k-settings.h"
+
+#define	SHUTTER_CMD_MASK	((1<<6)-1)
+#define	SHUTTER_CMD_UP		0b011001
+#define	SHUTTER_CMD_STOP	0b101010
+#define	SHUTTER_CMD_DOWN	0b001000
+
+class RF433Ctl {
+	void transmitX(uint64_t cmd);
+	void transmit_signal(uint16_t t_one_ms, uint16_t t_zero_ms);
+	const int txPin;
+public:
+	RF433Ctl(int _txPin) :
+			txPin(_txPin) {
+		pinMode(txPin, OUTPUT);
+		digitalWrite(txPin, 0);
+	}
+	void cmd(uint8_t id,int8_t direction);
+};
+
