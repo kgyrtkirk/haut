2e0f1fae4966b1caafe9a8c245d17a32a232720c
Zoltan Haindrich <zhaindrich@cloudera.com>
2020-04-28 15:39:04 +0200

    bb 04-27

--- busy_logic/src/bb_puzzle.h
+++ busy_logic/src/bb_puzzle.h
@@ -24,10 +24,10 @@ bField bb_puzzle(bField i) {
 #define i7 i.b7
 #define i8 i.b8
 
-  r.b4=i1 && i5 && i6;
-  r.b3=i2 ^ i4 ^ i7;
-  r.b2=i4 && i6 && !i7 && i8;
-  r.b1=i3 ^ i5 ;
+  r.b1=i1 ^ ( i7 && i8 && i6);
+  r.b2=i1 ^ i2 ^ ( !i2 ^ i4 ^ i7 ^i5 );
+  r.b3=i1 ^ i2 ^ i3;
+  r.b4=!i1 && !i2 && i3 && i4;
 
 if(false) {
   r.b1=!r.b1;
